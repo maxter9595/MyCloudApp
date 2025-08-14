c9278d2c36e9ae5572b241c67b4e198ec11279d4
Maxim Terletskii <max.t95@bk.ru>
2025-08-14 23:43:51 +0300

    Auto deploy test

--- frontend/src/pages/core/HomePage/index.jsx
+++ frontend/src/pages/core/HomePage/index.jsx
@@ -13,7 +13,7 @@ const HomePage = () => {
     <main className="home-page">
       <img src={Cloud} alt="Cloud" className="cloud-bg" />
       <h1>Добро пожаловать в MyCloud</h1>
-      <p> MyCloud - супер современное облачное хранилище для ваших файлов</p>
+      <p> MyCloud - современное облачное хранилище для ваших файлов</p>
 
       {!isAuthenticated && (
         <div className="auth-links">
