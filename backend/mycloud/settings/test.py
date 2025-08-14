273d76380fbf8172d8a2a35d4c55f0c548782be2
Maxim Terletskii <max.t95@bk.ru>
2025-08-15 00:02:55 +0300

    Auto deploy test

---
+++ backend/mycloud/settings/test.py
@@ -0,0 +1,22 @@
+from .local import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://localhost:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
