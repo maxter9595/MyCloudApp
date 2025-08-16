5cf3fad6eb92f27cb6f71733cbb7e10941909775
Maxim Terletskii <max.t95@bk.ru>
2025-08-16 20:50:01 +0300

    Test auto deploy

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,15 +11,6 @@ until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
     sleep 2
 done
 
-echo "Проверка существования БД $DB_NAME..."
-if ! psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -lqt | cut -d \| -f 1 | grep -qw "$DB_NAME"; then
-    echo "БД $DB_NAME не существует, создаю..."
-    createdb -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" "$DB_NAME"
-    echo "БД $DB_NAME успешно создана"
-else
-    echo "БД $DB_NAME уже существует"
-fi
-
 echo "Ожидание Redis..."
 until nc -z redis 6379; do
     sleep 2
